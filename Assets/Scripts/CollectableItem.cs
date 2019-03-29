f4e38a9416edb393977d140dcfe717b3655290be
Xenoxido <43368703+Xenoxido@users.noreply.github.com>
2019-03-29 13:55:29 +0100

    Coins&UI

---
+++ Assets/Scripts/CollectableItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour
+{
+    [SerializeField] private string itemName;
+    [SerializeField] private GameManager gameManager;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Item collected: " + itemName);
+        gameManager.SendMessage("CoinCollected");
+        Destroy(this.gameObject);
+    }
+}
