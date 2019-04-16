1af791ab1854326845900a111ced68ed38d1b9f3
T S <tschwecht@gmail.com>
2019-04-16 02:16:16 +0200

    created dino

--- Assets/Scripts/EnemyScripts/PatrolEnemy.cs
+++ Assets/Scripts/EnemyScripts/PatrolEnemy.cs
@@ -7,6 +7,8 @@ public class PatrolEnemy : MonoBehaviour
     private BoxCollider2D _boxCollider;
     private Rigidbody2D _body;
     public float speed = 4.5f;
+    public float runspeed = 9f;
+    private float currentSpeed;
     private bool right;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,7 @@ public class PatrolEnemy : MonoBehaviour
         _boxCollider = GetComponent<BoxCollider2D>();
         _body = GetComponent<Rigidbody2D>();
         right = true;
+        currentSpeed = speed;
         
         
     }  
@@ -32,7 +35,7 @@ public class PatrolEnemy : MonoBehaviour
                deltaX *= -1;              
           }
         Vector2 movement = Vector2.right;
-        movement = movement * deltaX * speed;      
+        movement = movement * deltaX * currentSpeed;      
         
         _body.velocity = movement;
     }
@@ -42,5 +45,18 @@ public class PatrolEnemy : MonoBehaviour
 
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         right = transform.localScale.x > 0;
+    }
+
+    private void run(bool shouldRun)
+    {
+        if (shouldRun)
+        {
+            currentSpeed = runspeed;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+
     }
 }
--- Assets/Scripts/EnemyScripts/Run.cs
+++ Assets/Scripts/EnemyScripts/Run.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Run : MonoBehaviour
+public class Run : MonoBehaviour    
 {
+    private Animator _anim;
+
+    public Transform playerDetection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        var localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody2D>().velocity);
+        Vector2 dir = new Vector2(localVelocity.x, 0);
+        dir.Normalize();
+        RaycastHit2D straightRay = Physics2D.Raycast(playerDetection.position, dir, 6f);        
+        if (straightRay.collider && straightRay.collider.tag == "Player")
+        {
+            if (_anim.GetBool("Sees") == false)
+            {
+                SendMessage("run", true);
+                _anim.SetBool("Sees", true);
+            }
+        }
+        else
+        {
+            if (_anim.GetBool("Sees") == true)
+            {
+                SendMessage("run", false);
+                _anim.SetBool("Sees", false);
+            }
+
+        }
     }
 }
